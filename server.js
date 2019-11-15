ec56acac2640ff27b6ef3cba29d7a98b1e4ae6fe
Randamus <rcviets@gmail.com>
2019-11-14 18:54:45 -0700

    initiate server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+//Dependecies
+let express = require('express');
+let path = require('path');
+let friends = require('./app/data/friends.js');
+
+//Set Up Express
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+//Data Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Import Files
+require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
+
+//Start Server
+app.listen(PORT, function() {
+    console.log('Listening on PORT: ' + PORT);
+});
