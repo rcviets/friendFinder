42c857cf90c3c28047dadc625ed9bb3463ca8d6e
Randamus <rcviets@gmail.com>
2019-11-14 20:06:19 -0700

    populated friend data

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+let friends = [{
+    'name': 'Dean Forester',
+    'photo': '/images/dean.PNG',
+    'scores': [
+        1,
+        1,
+        3,
+        5,
+        3,
+        1,
+        1,
+        5,
+        5,
+        3
+    ]
+},
+{
+    'name': 'Jess Mariano',
+    'photo': '/images/jess.PNG',
+    'scores': [
+        3,
+        3,
+        1,
+        1,
+        1,
+        5,
+        5,
+        3,
+        1,
+        1
+    ]
+},
+{
+    'name': 'Logan Huntzberger',
+    'photo': 'images/logan.PNG',
+    'scores': [
+        5,
+        5,
+        5,
+        1,
+        5,
+        3,
+        5,
+        1,
+        1,
+        5
+    ]
+}
+]
+
+module.exports = friends;
