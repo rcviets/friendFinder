a01ea1f6d60f5c35976df543b9cdc8888819564c
Randamus <rcviets@gmail.com>
2019-11-16 09:06:51 -0700

    fixed broken images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 let friends = [{
     'name': 'Dean Forester',
-    'photo': '/images/dean.PNG',
+    'photo': 'https://media1.popsugar-assets.com/files/thumbor/Fb9OJWrq1uoJUhF0en8-MHvAuoA/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2016/11/08/833/n/1922283/4895791c582220c64ad1f5.33276661_edit_img_cover_file_2387502_1478631498/i/Why-Dean-Best-Gilmore-Girls.jpg',
     'scores': [
         1,
         1,
@@ -16,7 +16,7 @@ let friends = [{
 },
 {
     'name': 'Jess Mariano',
-    'photo': '/images/jess.PNG',
+    'photo': 'https://hpathy.com/wp-content/uploads/2019/05/Jess-3.jpg',
     'scores': [
         3,
         3,
@@ -32,7 +32,7 @@ let friends = [{
 },
 {
     'name': 'Logan Huntzberger',
-    'photo': 'images/logan.PNG',
+    'photo': 'https://akns-images.eonline.com/eol_images/Entire_Site/201559/rs_634x1024-150609113635-634.matt-gilmore.cm.6815.jpg',
     'scores': [
         5,
         5,
